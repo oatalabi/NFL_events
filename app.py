d7c792016d5ec0b2af9c584e0ec8764280d78a7d
otalabi <oatalabi@gmail.com>
2020-08-22 16:09:56 -0700

    clean up code

--- app.py
+++ app.py
@@ -16,13 +16,43 @@ app.config['JSON_SORT_KEYS'] = False
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
-@app.errorhandler(Exception) #handle any error exception in external API
+@app.errorhandler(Exception) 
 def handle_error(error_code):
-    error_code = int(str(error_code))
+    try:
+        error_code = int(str(error_code)) #handle any error exception in external API
+    except:
+        error_code = 500 #handle any internal server error
+        
     response = {}
     response['message'] = 'An unexpected error has occurred.'
     response['status'] = error_code
     return jsonify(response), error_code
+
+def get_scoreboard_data(base_url, start_date, end_date):
+    scoreboard_api_url = f'{base_url}/scoreboard/NFL/{start_date}/{end_date}.json?api_key={API_KEY}'  
+
+    # handle error from external API call
+    try:
+        scoreboard_response = requests.get(scoreboard_api_url, headers={"Accept": "application/json"})
+        scoreboard_response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise Exception(scoreboard_response.status_code)
+
+    scoreboard_data = scoreboard_response.json()['results']
+    return scoreboard_data
+
+def get_team_rankings(base_url):
+    team_rankings_api_url = f'{base_url}/team_rankings/NFL.json?api_key={API_KEY}'    
+
+    # handle error from external API call
+    try:
+        team_rankings_response = requests.get(team_rankings_api_url, headers={"Accept": "application/json"})
+        team_rankings_response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise Exception(team_rankings_response.status_code)
+
+    team_rankings_data = team_rankings_response.json()['results']['data']
+    return team_rankings_data
 
 def combine_data(scoreboard, rankings):
     team_rankings = {}
@@ -75,28 +105,12 @@ def getEvents():
     start_date = request.args.get('start_date', default = default_date, type = str)
     end_date = request.args.get('end_date', default = default_date, type = str)
 
-    base_url = 'https://delivery.chalk247.com'
+    base_url = 'https://delivery.chalk247.com'   
 
-    scoreboard_api_url = f'{base_url}/scoreboard/NFL/{start_date}/{end_date}.json?api_key={API_KEY}'
-    team_rankings_api_url = f'{base_url}/team_rankings/NFL.json?api_key={API_KEY}'    
-
-    # handle error from first external API call
-    try:
-        scoreboard_data = requests.get(scoreboard_api_url, headers={"Accept": "application/json"})
-        scoreboard_data.raise_for_status()
-    except requests.exceptions.RequestException as err:
-        raise Exception(scoreboard_data.status_code)
-
-    # handle error from second external API call
-    try:
-        team_rankings_data = requests.get(team_rankings_api_url, headers={"Accept": "application/json"})
-        team_rankings_data.raise_for_status()
-    except requests.exceptions.RequestException as err:
-        raise Exception(team_rankings_data.status_code)
-
-    scoreboard_results = scoreboard_data.json()['results']
-    rankings_results = team_rankings_data.json()['results']['data']
+    scoreboard_results = get_scoreboard_data(base_url, start_date, end_date)
+    rankings_results = get_team_rankings(base_url)
     response_data = combine_data(scoreboard_results, rankings_results)
+
     return jsonify(response_data), 200
 
 # start the server
